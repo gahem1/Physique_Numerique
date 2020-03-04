67d242c5f0cc47fc5eda387823eb98c3852b4255
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-04 00:20:59 -0500

    Finished 4b.

--- TP2/algorithme_QR.py
+++ TP2/algorithme_QR.py
@@ -1,15 +1,26 @@
 import numpy as np
 
 
-def algo_QR(input_matrix: np.ndarray):
+def algo_qr(input_matrix: np.ndarray):
     n = len(input_matrix[0, :])
     if n == len(input_matrix[:, 0]):
-        q = np.zeros((n, n))
-        for i in range(n):
-            a = np.tile(input_matrix[:, i], (n, 1)).T
-            q[:, i] = input_matrix[:, i] - np.multiply(np.tile(np.sum(np.multiply(a, q), axis=0), (n, 1)), q)
-            q[:, i] = np.divide(q[:, i], np.sqrt(np.sum(q[:, i])))
-
+        q, r = np.zeros((n, n)), np.zeros((n, n))
+        u = input_matrix[:, 0]
+        r[0, 0] = np.sqrt(np.sum(u * u))
+        q[:, 0] = u / r[0, 0]
+        for i in range(1, n):
+            a = np.tile(input_matrix[:, i], (i, 1)).T
+            r[:i, i] = np.sum(a * q[:, :i], axis=0)
+            u = input_matrix[:, i] - np.sum(np.tile(r[:i, i], (n, 1)) * q[:, :i], axis=1)
+            r[i, i] = np.sqrt(np.sum(u * u))
+            q[:, i] = u / r[i, i]
+        return q, r
     else:
         print("The input needs to be a square matrix")
         quit()
+
+
+if __name__ == "__main__":
+    B = np.array([[1, 4, 8, 4], [4, 2, 3, 7], [8, 3, 6, 9], [4, 7, 9, 2]])
+    Q, R = algo_qr(B)
+    print(Q @ R)
