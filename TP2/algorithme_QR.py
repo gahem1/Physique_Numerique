fbb34f597762d34921abdb5c371458f126d1ab4d
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-03 22:22:12 -0500

    Made Q, need to make R

--- TP2/algorithme_QR.py
+++ TP2/algorithme_QR.py
@@ -7,8 +7,9 @@ def algo_QR(input_matrix: np.ndarray):
         q = np.zeros((n, n))
         for i in range(n):
             a = np.tile(input_matrix[:, i], (n, 1)).T
-            substract = np.sum(np.multiply(a, q))
-            q[:, i] = input_matrix[:, i]
+            q[:, i] = input_matrix[:, i] - np.multiply(np.tile(np.sum(np.multiply(a, q), axis=0), (n, 1)), q)
+            q[:, i] = np.divide(q[:, i], np.sqrt(np.sum(q[:, i])))
+
     else:
         print("The input needs to be a square matrix")
         quit()
