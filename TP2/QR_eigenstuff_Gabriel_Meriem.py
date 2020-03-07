76a995e3c32c709d57a806e7be676bf34d69fdc0
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-06 20:22:54 -0500

    Closing time

--- TP2/QR_eigenstuff_Gabriel_Meriem.py
+++ TP2/QR_eigenstuff_Gabriel_Meriem.py
@@ -1,14 +1,10 @@
 import numpy as np
 from algorithme_QR_Gabriel_Meriem import algo_qr, B
-from time import time
 
 
 def QR_eigenstuff(matrix, accuracy):
     n = len(matrix[0, :])
     v = np.eye(n)
-    if accuracy <= 10 ** -16:
-        matrix = matrix.astype(np.longdouble)
-        v = matrix.astype(np.longdouble)
 
     verify_accuracy = np.ones((n, n), dtype=bool) ^ np.eye(n, dtype=bool)
     while np.any(abs(matrix[verify_accuracy]) > accuracy):
@@ -19,21 +15,12 @@ def QR_eigenstuff(matrix, accuracy):
 
 if __name__ == "__main__":
     error = 10 ** -6
-    timer = time()
+    print("Pour une erreur de {}".format(error))
     vep, vap = QR_eigenstuff(B, error)
     print(vep)
     print(vap)
-    print("L'algorithme avec une erreur de {0} a pris {1:f} secondes".format(error, time() - timer))
     error = 10 ** -12
-    timer = time()
+    print("Pour une erreur de {}".format(error))
     vep, vap = QR_eigenstuff(B, error)
     print(vep)
     print(vap)
-    print("L'algorithme avec une erreur de {0} a pris {1:f} secondes".format(error, time() - timer))
-    error = 10 ** -18
-    print(error)
-    timer = time()
-    vep, vap = QR_eigenstuff(B, error)
-    print(vep)
-    print(vap)
-    print("L'algorithme avec une erreur de {0} a pris {1:f} secondes".format(error, time() - timer))
