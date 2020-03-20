19788a41623654a17108d529b24d5a5ac482d733
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-19 23:48:28 -0400

    Cleaner

--- TP3/multi_amplitudes_Gabriel_Meriem.py
+++ TP3/multi_amplitudes_Gabriel_Meriem.py
@@ -1,8 +1,10 @@
 from oscillateur_harmonique_Gabriel_Meriem import *
 
 if __name__ == "__main__":
+    om = 1
+
     plt.figure()
-    plt.plot(tvals, rk4(N, xi, xpi, ti)[0], tvals, rk4(N, 2 * xi, xpi, ti)[0], tvals, rk4(N, 3 * xi, xpi, ti)[0],
+    plt.plot(tvals, rk4(N, xi, xpi, om)[0], tvals, rk4(N, 2 * xi, xpi, om)[0], tvals, rk4(N, 3 * xi, xpi, om)[0],
              linewidth=2)
     plt.ylabel("x(t)", fontsize=18)
     plt.xlabel("t", fontsize=18)
--- TP3/oscillateur_harmonique_Gabriel_Meriem.py
+++ TP3/oscillateur_harmonique_Gabriel_Meriem.py
@@ -2,9 +2,10 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def rk4(n, x1, x2, t):
+def rk4(n, x1, x2, omega):
     xlist = np.empty(n)
     xplist = np.empty(n)
+    t = 0
     for i in range(n):
         xlist[i] = x1
         xplist[i] = x2
@@ -15,10 +16,10 @@ def rk4(n, x1, x2, t):
         r3 = step * (x2 + 0.5 * r2)
         r4 = step * (x2 + r3)
 
-        k1 = step * xpp(x1)
-        k2 = step * xpp(x1 + 0.5 * k1)
-        k3 = step * xpp(x1 + 0.5 * k2)
-        k4 = step * xpp(x1 + k3)
+        k1 = step * xpp(x1, omega)
+        k2 = step * xpp(x1 + 0.5 * k1, omega)
+        k3 = step * xpp(x1 + 0.5 * k2, omega)
+        k4 = step * xpp(x1 + k3, omega)
 
         x1 += (r1 + 2 * r2 + 2 * r3 + r4) / 6
         x2 += (k1 + 2 * k2 + 2 * k3 + k4) / 6
@@ -26,22 +27,20 @@ def rk4(n, x1, x2, t):
     return xlist, xplist
 
 
-def xpp(x1):
+def xpp(x1, omega):
     return -x1 * (omega ** 2)
 
 
 N = 100000
-ti = 0
 tf = 50
-tvals = np.linspace(ti, tf, num=N)
-step = (tf - ti) / N
-
-omega = 1
-xi = 1
-xpi = 0
+tvals = np.linspace(0, tf, num=N)
+step = tf / N
 
 if __name__ == "__main__":
-    xvals, xpvals = rk4(N, xi, xpi, ti)
+    xi = 1
+    xpi = 0
+    angular_frequency = 1
+    xvals, xpvals = rk4(N, xi, xpi, angular_frequency)
 
     plt.figure()
     plt.plot(tvals, xvals, linewidth=2)
