ae66c33f361658ccc70b7fda8483fb2de1bcaab6
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-19 21:32:07 -0400

    Update oscillateur_harmonique_Gabriel_Meriem.py

--- TP3/oscillateur_harmonique_Gabriel_Meriem.py
+++ TP3/oscillateur_harmonique_Gabriel_Meriem.py
@@ -3,11 +3,10 @@ import numpy as np
 
 
 def xpp(x1):
-    """"""
-    return -omega ** 2 * x1
+    return -x1 * (omega ** 2)
 
 
-N = 1000
+N = 100000
 t = 0
 tf = 50
 tvals = np.linspace(t, tf, num=N)
@@ -22,15 +21,22 @@ for i in range(N):
     xvals[i] = x
     t += step
 
-    k1 = step * xpp(x)
     r1 = step * xp
-    k2 = step * xpp(x + 0.5 * r1)
-    r2 = step * (xp + 0.5 * k1)
-    k3 = step * xpp(x + 0.5 * r2)
-    r3 = step * (xp + 0.5 * k2)
-    k4 = step * xpp(x + k3)
+    r2 = step * (xp + 0.5 * r1)
+    r3 = step * (xp + 0.5 * r2)
     r4 = step * (xp + r3)
+    x += (r1 + 2 * r2 + 2 * r3 + r4) / 6
 
-    x += (k1 + 2 * k2 + 2 * k3 + k4) / 6
-    xp += (r1 + 2 * r2 + 2 * r3 + r4) / 6
+    k1 = step * xpp(x)
+    k2 = step * xpp(x + 0.5 * k1)
+    k3 = step * xpp(x + 0.5 * k2)
+    k4 = step * xpp(x + k3)
+    xp += (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
+plt.figure()
+plt.plot(tvals, xvals, linewidth=2)
+plt.ylabel("x(t)", fontsize=18)
+plt.xlabel("t", fontsize=18)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.show()
