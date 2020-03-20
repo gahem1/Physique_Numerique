d0875b829637a1f433df05db47c81ce9412c38c7
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-20 17:03:45 -0400

    Sent bobs

--- TP3/phase_space_comparison_Gabriel_Meriem.py
+++ TP3/phase_space_comparison_Gabriel_Meriem.py
@@ -7,18 +7,18 @@ if __name__ == "__main__":
     xpi = 0
 
     xharm1, xpharm1 = rk4(N, xi, xpi, omega, xpp)
-    xharm2, xpharm2 = rk4(N, xi * 2, xpi, omega, xpp)
-    xanharm1, xpanharm1 = rk4(N, xi * 0.5, xpi, omega, xpp)
-    xanharm2, xpanharm2 = rk4(N, xi * 3, xpi, omega, xpp)
+    xanharm1, xpanharm1 = rk4(N, xi, xpi, omega, newxpp)
+    xharm2, xpharm2 = rk4(N, xi * 1.5, xpi, omega, xpp)
+    xanharm2, xpanharm2 = rk4(N, xi * 1.5, xpi, omega, newxpp)
 
     plt.figure()
-    plt.plot(xharm1, xpharm1, xharm2, xpharm2, xanharm1, xpanharm1, xanharm2, xpanharm2)
+    plt.plot(xharm1, xpharm1, xanharm1, xpanharm1, xharm2, xpharm2, xanharm2, xpanharm2, linewidth=0.5)
     plt.ylabel("xp", fontsize=18)
     plt.xlabel("x", fontsize=18)
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
     plt.gca().set_aspect('equal', adjustable='box')
-    plt.legend(["Harmonique avec x(0)={}".format(xi), "Harmonique avec x(0)={}".format(2 * xi),
-                "Anharmonique avec x(0)={}".format(0.5 * xi), "Anharmonique avec x(0)={}".format(3 * xi)],
+    plt.legend(["Harmonique avec x(0)={}".format(xi), "Anharmonique avec x(0)={}".format(0.5 * xi),
+               "Harmonique avec x(0)={}".format(2 * xi), "Anharmonique avec x(0)={}".format(3 * xi)],
                loc="upper right", fontsize=8)
     plt.show()
