58796e924931a3158633bf7497b8fd3230370522
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-21 14:40:41 -0400

    Update mecanique_celeste_Gabriel_Meriem.py

--- TP3/mecanique_celeste_Gabriel_Meriem.py
+++ TP3/mecanique_celeste_Gabriel_Meriem.py
@@ -2,38 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def gravity(x1, x2, x3):
-    if x1 != x2:
-        f12 = (x1 - x2) / np.abs(x1 - x2) ** 3
-    else:
-        f12 = 0
-    if x1 != x3:
-        f13 = (x1 - x3) / np.abs(x1 - x3) ** 3
-    else:
-        f13 = 0
-    if x2 != x3:
-        f23 = (x2 - x3) / np.abs(x2 - x3) ** 3
-    else:
-        f23 = 0
-    ax1 = -G * (m2 * f12 + m3 * f13)
-    ax2 = -G * (m1 * (-f12) + m3 * f23)
-    ax3 = -G * (m1 * (-f13) + m2 * (-f23))
-    return np.array([ax1, ax2, ax3])
+def gravity(x1, x2, x3, d):
+    """The forces exerted on each body. d[0] is the distance between 1&2, d[1] between 1&3, and d[2] between 2&3"""
+    ax1 = m2 * (x1 - x2) / d[0] ** 3 + m3 * (x1 - x3) / d[1] ** 3
+    ax2 = m1 * (x2 - x1) / d[0] ** 3 + m3 * (x2 - x3) / d[2] ** 3
+    ax3 = m1 * (x3 - x1) / d[1] ** 3 + m2 * (x3 - x2) / d[2] ** 3
+    return -G * np.array([ax1, ax2, ax3])
 
 
-def leapfrog(N, tf, x1, x2, x3, y1, y2, y3, vx1, vx2, vx3, vy1, vy2, vy3):
-    t, dt = 0, tf / N
+def leapfrog(n, tf, x1, x2, x3, y1, y2, y3, vx1, vx2, vx3, vy1, vy2, vy3):
+    t, dt = 0, tf / n
 
-    xlist, ylist = np.empty([N, 3]), np.empty([N, 3]),
+    xlist, ylist, vx, vy = np.empty([n, 3]), np.empty([n, 3]), np.empty([2, 3]), np.empty([2, 3])
+
     xlist[0, :], ylist[0, :] = np.array([x1, x2, x3]), np.array([y1, y2, y3])
+    vx[0, :], vy[0, :] = np.array([vx1, vx2, vx3]), np.array([vy1, vy2, vy3])
 
-    vx, vy = np.array([vx1, vx2, vx3]), np.array([vy1, vy2, vy3])
+    for i in range(n - 1):
+        r1 = np.sqrt((xlist[i + 1, 0] - xlist[i + 1, 1]) ** 2 + (ylist[i + 1, 0] - ylist[i + 1, 1]) ** 2)
+        r2 = np.sqrt((xlist[i + 1, 0] - xlist[i + 1, 2]) ** 2 + (ylist[i + 1, 0] - ylist[i + 1, 2]) ** 2)
+        r3 = np.sqrt((xlist[i + 1, 1] - xlist[i + 1, 2]) ** 2 + (ylist[i + 1, 1] - ylist[i + 1, 2]) ** 2)
+        dist = np.array([r1, r2, r3])
 
-    for i in range(N - 2):
-        vx += gravity(x1, x2, x3) * dt
-        xlist[i + 1, :] = xlist[i, :] + vx * dt
-        vy += gravity(y1, y2, y3) * dt
-        ylist[i + 1, :] = ylist[i, :] + vy * dt
+        vx[i % 2, :] += gravity(xlist[i + 1, 0], xlist[i + 1, 1], xlist[i + 1, 2], dist) * dt
+        xlist[i + 2, :] = xlist[i, :] + vx[(i + 1) % 2, :] * dt
+        vy[i % 2, :] += gravity(ylist[i + 1, 0], ylist[i + 1, 1], ylist[i + 1, 2], dist) * dt
+        ylist[i + 2, :] = ylist[i, :] + vy[(i + 1) % 2, :] * dt
 
     return xlist, ylist
 
