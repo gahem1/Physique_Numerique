b93ffe3b3af2a364fb51eee2bbc150e34d17763d
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-03-28 23:17:05 -0400

    Update relaxation_cylinder_Gabriel_Meriem.py

--- TP4/relaxation_cylinder_Gabriel_Meriem.py
+++ TP4/relaxation_cylinder_Gabriel_Meriem.py
@@ -8,15 +8,15 @@ class Cylinder:
         self.step = step
         self.previous = None
         num_r = int((r_max.max() - r_min) / step)
-        num_z = int((z_bounds[-1] - z_bounds[1]) / step + 2)
+        num_z = int((z_bounds[-1] - z_bounds[0]) / step + 2)
         self.grid = np.zeros((num_r, num_z))
         self.unfixed = np.zeros((num_r, num_z), dtype=bool)
-        self.rmat = np.tile(np.arange(r_min, r_max, step), num_z).T
+        self.rmat = np.tile(np.arange(r_min, r_max, step), (num_z, 1)).T
         for i in range(len(r_max)):
             r = int((r_max[i] - r_min) / step)
-            z1, z2 = int((z_bounds[i] - z_bounds[0]) / step + 1), int((z_bounds[i + 1] - z_bounds[i]) / step + 1)
-            self.grid[1:r, z1:z2 + 1] = np.tile(np.arange(r - 1, 1, step) * v_mid / r, (z2 - z1 + 1, 1)).T
-            self.unfixed[1:r, z1:z2 + 1] = np.ones((r - 1, z2 - z1 + 1), dtype=bool)
+            z1, z2 = int((z_bounds[i] - z_bounds[0]) / step + 1), int((z_bounds[i + 1] - z_bounds[i]) / step + 2)
+            self.grid[1:r, z1:z2] = np.tile(np.arange(r, 1, -1) * v_mid / r, (z2 - z1, 1)).T
+            self.unfixed[1:r, z1:z2] = np.ones((r - 1, z2 - z1), dtype=bool)
 
     def iterate(self):
         self.previous, new_grid = np.copy(self.grid), np.copy(self.grid)
@@ -31,3 +31,7 @@ class Cylinder:
 
         self.iteration += 1
         self.grid = np.where(self.unfixed, new_grid, self.previous)
+
+
+if __name__ == "__main__":
+    test = Cylinder(1, 150, np.array([10]), np.array([0, 30]), 0.01)
