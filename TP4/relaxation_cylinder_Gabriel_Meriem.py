e15ca3d12e807a1f4fbc07ece4cc75dbfa17d79a
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-04-04 18:54:53 -0400

    Update relaxation_cylinder_Gabriel_Meriem.py

--- TP4/relaxation_cylinder_Gabriel_Meriem.py
+++ TP4/relaxation_cylinder_Gabriel_Meriem.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 
 class Cylinder:
@@ -17,17 +20,18 @@ class Cylinder:
         for i in range(len(r_max)):
             r = int((r_max[i] - r_min) * 2 / step + 2)
             z1, z2 = int((z_bounds[i] - z_bounds[0]) / step + 1), int((z_bounds[i + 1] - z_bounds[0]) / step + 2)
-            self.grid[1:r, z1:z2] = np.tile(np.arange(r - 1, -1, -1) * v_mid / r, (z2 - z1, 1)).T
-            self.grid[r:, z1:z2] = 0
+            self.grid[1:r, z1:z2] = np.tile(np.arange(r - 1, 0, -1) * v_mid / r, (z2 - z1, 1)).T
+            self.grid[r - 1:, z1:z2] = 0
             self.unfixed[2:r - 2, z1:z2] = np.ones((r - 4, z2 - z1), dtype=bool)
+
         self.grid[0:2, :] = v_mid
 
     def iterate(self):
         self.previous, new_grid = np.copy(self.grid), np.copy(self.grid)
 
-        gr1 = self.grid[4:, 1:-1] + self.grid[:-4, 1:-1] + self.grid[2:-2, 2:], self.grid[2:-2, :-2]
+        gr1 = self.grid[4:, 1:-1] + self.grid[:-4, 1:-1] + self.grid[2:-2, 2:] + self.grid[2:-2, :-2]
         gr2 = (self.grid[3:-1, 1:-1] - self.grid[1:-3, 1:-1]) * self.rmat
-        new_grid[1:-1, 1:-1] = (gr1 + gr2) / 4
+        new_grid[2:-2, 1:-1] = (gr1 + gr2) / 4
 
         self.iteration += 1
         self.grid = np.where(self.unfixed, new_grid, self.previous)
@@ -35,10 +39,20 @@ class Cylinder:
 
 
 if __name__ == "__main__":
-    err = 2 * 10 ** -8
-    test = Cylinder(1, 150, np.array([10]), np.array([0, 30]), 1.25 * 10 ** -5, err)
-    while test.error >= err:
-        test.iterate()
+    err = 0.1
+    h = 2 * err
+    cyl = Cylinder(1, 150, np.array([10]), np.array([0, 30]), h, err)
+    while cyl.error >= err:
+        cyl.iterate()
 
-    print(test.error)
-    print(test.iteration)
+    print(cyl.iteration)
+    print(cyl.grid[-2:, 80])
+    cyl = pd.DataFrame(cyl.grid[1:, 1:-1], np.arange(1, 10 + h, h / 2), np.arange(0, 30 + h, h))
+    ax = sns.heatmap(cyl, cbar_kws={'label': 'Voltage [V]'})
+    ax.set_xlabel("z [cm]", fontsize=18)
+    ax.set_ylabel("r [cm]", fontsize=18)
+    ax.set_xticks(np.arange(0.5, 175.5, 25))
+    ax.set_xticklabels(np.arange(0, 35, 5))
+    ax.set_yticks(np.arange(0, 45, 5))
+    ax.set_yticklabels(np.arange(1, 11, 1))
+    plt.show()
