2f6aa8a84e245b0d45ec3c58735e3da16bbda5ee
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-04-05 15:09:09 -0400

    Update relaxation_cylinder_Gabriel_Meriem.py

--- TP4/relaxation_cylinder_Gabriel_Meriem.py
+++ TP4/relaxation_cylinder_Gabriel_Meriem.py
@@ -14,26 +14,26 @@ class Cylinder:
         num_z = int((z_bounds[-1] - z_bounds[0]) / step + 3)
         self.grid = np.zeros((num_r, num_z))
         self.unfixed = np.zeros((num_r, num_z), dtype=bool)
-        self.rmat = np.tile(np.arange(step / 2, r_max.max() + step, step / 2), (num_z, 1)).T
-        self.rmat = self.step / self.rmat[:-1, 1:-1]
+        self.rmat = np.tile(np.arange(0, r_max.max() + step, step / 2), (num_z, 1)).T
+        self.rmat = self.step / self.rmat[2:-2, 1:-1]
         r1 = int(r_min * 2 / step + 1)
         for i in range(len(r_max)):
             r2 = int((r_max[i]) * 2 / step + 2)
             z1, z2 = int((z_bounds[i] - z_bounds[0]) / step + 1), int((z_bounds[i + 1] - z_bounds[0]) / step + 2)
             self.grid[r1:r2, z1:z2] = np.tile(np.arange(r2 - r1, 0, -1) * v_mid / (r2 - r1), (z2 - z1, 1)).T
             self.grid[r2 - 1:, z1:z2] = 0
-            self.unfixed[r1:r2 - 2, z1:z2] = bool(1)
+            self.unfixed[:r2 - 2, z1:z2] = bool(1)
 
         z1, z2 = int((ztige[0] - z_bounds[0]) / step), int((z_bounds[-1] - ztige[-1]) / step + 1)
         self.grid[0:r_min, z1:z2] = v_mid
-        self.unfixed[:, 1:z1], self.unfixed[:, z2:-1] = bool(1), bool(1)
+        self.unfixed[:r1 + 1, z1:z2] = bool(0)
 
     def iterate(self):
         self.previous, new = np.copy(self.grid), np.copy(self.grid)
 
-        new[1:-1, 1:-1] = self.grid[3:-1, 1:-1] + self.grid[1:-3, 1:-1] + self.grid[1:-1, 2:] + self.grid[1:-1, :-2]
-        gr2 = (self.grid[1:, 1:-1] - self.grid[:-1, 1:-1]) * self.rmat
-        new[:, 1:-1] = (gr1 + gr2) / 4
+        gr1 = self.grid[4:, 1:-1] + self.grid[:-4, 1:-1] + self.grid[2:-2, 2:] + self.grid[2:-2, :-2]
+        gr2 = (self.grid[3:-1, 1:-1] - self.grid[1:-3, 1:-1]) * self.rmat
+        new[2:-2, 1:-1] = (gr1 + gr2) / 4
 
         self.iteration += 1
         self.grid = np.where(self.unfixed, new, self.previous)
@@ -43,7 +43,7 @@ class Cylinder:
 if __name__ == "__main__":
     err = 0.01
     h = 2 * err
-    cyl = Cylinder(1, 150, np.array([10]), np.array([0, 30]), h, err)
+    cyl = Cylinder(1, 150, np.array([10]), np.array([0, 30]), h, err, np.array([0, 30]))
     while cyl.error >= err:
         cyl.iterate()
 
