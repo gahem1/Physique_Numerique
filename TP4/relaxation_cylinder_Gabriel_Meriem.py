870f594f46ae044376fbac818cb7fe53b973a538
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-04-05 00:11:40 -0400

    Update relaxation_cylinder_Gabriel_Meriem.py

--- TP4/relaxation_cylinder_Gabriel_Meriem.py
+++ TP4/relaxation_cylinder_Gabriel_Meriem.py
@@ -1,58 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import seaborn as sns
 
 
 class Cylinder:
-    def __init__(self, r_min: float, v_mid: float, r_max: np.ndarray, z_bounds: np.ndarray, step: float, error: float):
+    def __init__(self, r_min, v_mid, r_max, z_bounds, step, error, ztige):
         self.iteration = 0
         self.v_mid = v_mid
         self.step = step
         self.previous = None
         self.error = error + 1
-        num_r = int((r_max.max() - r_min) * 2 / step + 3)
+        num_r = int(r_max.max() * 2 / step + 2)
         num_z = int((z_bounds[-1] - z_bounds[0]) / step + 3)
         self.grid = np.zeros((num_r, num_z))
         self.unfixed = np.zeros((num_r, num_z), dtype=bool)
-        self.rmat = np.tile(np.arange(r_min - step / 2, r_max.max() + step, step / 2), (num_z, 1)).T
-        self.rmat = self.step / self.rmat[2:-2, 1:-1]
+        self.rmat = np.tile(np.arange(step / 2, r_max.max() + step, step / 2), (num_z, 1)).T
+        self.rmat = self.step / self.rmat[:-1, 1:-1]
+        r1 = int(r_min * 2 / step + 1)
         for i in range(len(r_max)):
-            r = int((r_max[i] - r_min) * 2 / step + 2)
+            r2 = int((r_max[i]) * 2 / step + 2)
             z1, z2 = int((z_bounds[i] - z_bounds[0]) / step + 1), int((z_bounds[i + 1] - z_bounds[0]) / step + 2)
-            self.grid[1:r, z1:z2] = np.tile(np.arange(r - 1, 0, -1) * v_mid / r, (z2 - z1, 1)).T
-            self.grid[r - 1:, z1:z2] = 0
-            self.unfixed[2:r - 2, z1:z2] = np.ones((r - 4, z2 - z1), dtype=bool)
+            self.grid[r1:r2, z1:z2] = np.tile(np.arange(r2 - r1, 0, -1) * v_mid / (r2 - r1), (z2 - z1, 1)).T
+            self.grid[r2 - 1:, z1:z2] = 0
+            self.unfixed[r1:r2 - 2, z1:z2] = bool(1)
 
-        self.grid[0:2, :] = v_mid
+        z1, z2 = int((ztige[0] - z_bounds[0]) / step), int((z_bounds[-1] - ztige[-1]) / step + 1)
+        self.grid[0:r_min, z1:z2] = v_mid
+        self.unfixed[:, 1:z1], self.unfixed[:, z2:-1] = bool(1), bool(1)
 
     def iterate(self):
-        self.previous, new_grid = np.copy(self.grid), np.copy(self.grid)
+        self.previous, new = np.copy(self.grid), np.copy(self.grid)
 
-        gr1 = self.grid[4:, 1:-1] + self.grid[:-4, 1:-1] + self.grid[2:-2, 2:] + self.grid[2:-2, :-2]
-        gr2 = (self.grid[3:-1, 1:-1] - self.grid[1:-3, 1:-1]) * self.rmat
-        new_grid[2:-2, 1:-1] = (gr1 + gr2) / 4
+        new[1:-1, 1:-1] = self.grid[3:-1, 1:-1] + self.grid[1:-3, 1:-1] + self.grid[1:-1, 2:] + self.grid[1:-1, :-2]
+        gr2 = (self.grid[1:, 1:-1] - self.grid[:-1, 1:-1]) * self.rmat
+        new[:, 1:-1] = (gr1 + gr2) / 4
 
         self.iteration += 1
-        self.grid = np.where(self.unfixed, new_grid, self.previous)
+        self.grid = np.where(self.unfixed, new, self.previous)
         self.error = np.absolute(self.grid - self.previous).max()
 
 
 if __name__ == "__main__":
-    err = 0.1
+    err = 0.01
     h = 2 * err
     cyl = Cylinder(1, 150, np.array([10]), np.array([0, 30]), h, err)
     while cyl.error >= err:
         cyl.iterate()
 
     print(cyl.iteration)
-    print(cyl.grid[-2:, 80])
-    cyl = pd.DataFrame(cyl.grid[1:, 1:-1], np.arange(1, 10 + h, h / 2), np.arange(0, 30 + h, h))
-    ax = sns.heatmap(cyl, cbar_kws={'label': 'Voltage [V]'})
-    ax.set_xlabel("z [cm]", fontsize=18)
-    ax.set_ylabel("r [cm]", fontsize=18)
-    ax.set_xticks(np.arange(0.5, 175.5, 25))
-    ax.set_xticklabels(np.arange(0, 35, 5))
-    ax.set_yticks(np.arange(0, 45, 5))
-    ax.set_yticklabels(np.arange(1, 11, 1))
+    ax = sns.heatmap(cyl.grid[-1:1:-1, 1:-1], cbar_kws={'label': 'Voltage [V]'})
+    ax.set_xlabel("z [cm]", fontsize=20)
+    ax.set_ylabel("r [cm]", fontsize=20)
+    ax.set_xticks(np.arange(0.5, 1750.5, 250))
+    ax.set_xticklabels(np.arange(0, 35, 5), fontsize=18)
+    ax.set_yticks(np.arange(0, 1000, 100))
+    ax.set_yticklabels(np.arange(10, 0, -1), fontsize=18)
+    ax.figure.axes[-1].yaxis.label.set_size(20)
     plt.show()
