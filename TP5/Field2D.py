01e6c0065d34cfd4ca5e85942876ac5c7fd86da7
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-04-16 23:38:16 -0400

    Update Field2D.py

--- TP5/Field2D.py
+++ TP5/Field2D.py
@@ -3,12 +3,15 @@ from matplotlib import pyplot as plt
 from PIL import Image
 from time import time
 
+
 I = complex(0,1)
+
+
 class Field2D:
     def __init__(self, ds:float, wavelength:float, N:int = None, array2D:ndarray = None):
         self.dx = ds
         self.dy = ds
-        self.wavelength= wavelength
+        self.wavelength = wavelength
         if array2D is None and N is not None:
             self.values = zeros((N, N),dtype=cdouble)
         elif array2D is not None and N is None:
@@ -25,12 +28,12 @@ class Field2D:
 
     @property
     def x(self) -> ndarray:
-        (N,dummy) = self.values.shape
+        (N, dummy) = self.values.shape
         return self.dx*linspace(-N/2, N/2, num=N, endpoint=False)
 
     @property
     def y(self) -> ndarray:
-        (dummy,N) = self.values.shape
+        (dummy, N) = self.values.shape
         return self.dy*linspace(-N/2, N/2, num=N, endpoint=False)
 
     def showIntensity(self):
@@ -53,7 +56,8 @@ class Field2D:
             for j, y in enumerate(self.y):
                 Ro = sqrt((x - xlist) ** 2 + (y - ylist) ** 2 + distance ** 2)
                 # we don't divide by r because we keep everything normalized
-                Efield[i, j] += sum(A * exp(-I * 2 * pi * Ro / self.wavelength))
+                Efield[i, j] += sum(A * exp(-I * 2 * pi * Ro / self.wavelength) / Ro)
+        self.values = Efield * self.dx * self.dy
 
     @classmethod
     def Gaussian(self, ds:float, N:int, width:float, wavelength:float, amplitude:float = 1.0):
@@ -61,18 +65,16 @@ class Field2D:
         allYs = ds*linspace(-N/2, N/2, num=N, endpoint=False)
 
         values = zeros((N, N),dtype=cdouble)
-        for (i,x) in enumerate(allXs):
-            for (j,y) in enumerate(allYs):
-                values[i,j] = amplitude*exp(-(x*x+y*y)/(width*width))
+        for (i, x) in enumerate(allXs):
+            for (j, y) in enumerate(allYs):
+                values[i, j] = amplitude*exp(-(x*x+y*y)/(width*width))
         return Field2D(array2D=values, ds=ds, wavelength=wavelength)
 
 
 if __name__ == "__main__":
     f = Field2D.Gaussian(width=10, amplitude=16.0, ds=0.1, N=250, wavelength=2)
     f.showIntensity()
-    W = max(f.values)
-    print(W)
     temps = time()
-    f.propagate(10)
+    f.propagate(5)
     print(time() - temps)
     f.showIntensity()
