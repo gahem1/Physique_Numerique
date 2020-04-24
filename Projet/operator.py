81bc138ac5387ddb695b83308de5303b8afa805a
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-04-24 19:57:19 -0400

    Update operator.py

--- Projet/operator.py
+++ Projet/operator.py
@@ -1,20 +1,42 @@
-from numpy import *
+import numpy as np
 
 
 class Operator:
-    def __init__(self, dim: int, alpha: float, beta: float, num: int):
+    def __init__(self, dim: int, alpha: float, beta: float):
         self.N = dim
-        self.matrix = zeros((dim, dim))
+        self.matrix = np.zeros((dim, dim))
+
+        fac = np.arange(1, dim - 3)
+        self.matrix[4:, :-4] += np.diag(beta * np.sqrt(fac * (fac + 1) * (fac + 2) * (fac + 3)))
+
+        fac = np.arange(1, dim - 2)
+        self.matrix[3:, :-3] += np.diag(alpha * np.sqrt(fac * (fac + 1) * (fac + 2)))
+
+        fac = np.arange(dim - 1)
+        ptwo +=
+
+        pone +=
+
+        n +=
+
+        mone +=
+
+        mtwo +=
+
+        mthree +=
+
+        mfour +=
+
 
     def algo_qr(self):
         q, r = np.zeros((self.N, self.N)), np.zeros((self.N, self.N))
-        u = copy(self.matrix[:, 0])
-        r[0, 0] = sqrt(sum(u * u))
+        u = np.copy(self.matrix[:, 0])
+        r[0, 0] = np.sqrt(sum(u * u))
         q[:, 0] = u / r[0, 0]
         for i in range(1, self.N):
-            a = tile(self.matrix[:, i], (i, 1)).T
-            r[:i, i] = sum(a * q[:, :i], axis=0)
-            u = self.matrix[:, i] - sum(tile(r[:i, i], (self.N, 1)) * q[:, :i], axis=1)
-            r[i, i] = sqrt(sum(u * u))
+            a = np.tile(self.matrix[:, i], (i, 1)).T
+            r[:i, i] = np.sum(a * q[:, :i], axis=0)
+            u = self.matrix[:, i] - np.sum(np.tile(r[:i, i], (self.N, 1)) * q[:, :i], axis=1)
+            r[i, i] = np.sqrt(np.sum(u * u))
             q[:, i] = u / r[i, i]
         return q, r
