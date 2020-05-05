969f263791acf9f42a6d0a14f06a261fb94ccb23
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-05-05 15:25:56 -0400

    Update orthogonality_test.py

--- Projet/orthogonality_test.py
+++ Projet/orthogonality_test.py
@@ -1,27 +1,28 @@
 from hamiltonian import Operator
-from random import random, randint
 import numpy as np
+from random import random, randint
 
 print("Gram-Schmidt")
-a = 6
+a = randint(5, 10)
 test = Operator(a, random(), random())
-va, ve, di, gg, te = test.eigenalgo(10 ** -14, 10000, "Gram-Schmidt")
+va, ve, di, gg, te = test.eigenalgo(10 ** -14, 20000, "Gram-Schmidt")
 for i in range(a):
-    print(np.sum(np.tile(ve[i, :], (a, 1)).T * ve, axis=0))
-    print(np.abs(ve - test.matrix @ ve))
+    print(np.sum(np.tile(ve[:, i], (a, 1)).T * ve, axis=0))
+print(np.abs(ve @ np.diag(va) - test.matrix @ ve))
 
 print("Givens")
-a = 6
+a = randint(5, 10)
 test = Operator(a, random(), random())
-va, ve, di, gg, te = test.eigenalgo(10 ** -14, 10000, "Givens")
+va, ve, di, gg, te = test.eigenalgo(10 ** -14, 20000, "Givens")
 for i in range(a):
-    print(np.sum(np.tile(ve[i, :], (a, 1)).T * ve, axis=0))
-    print(np.abs(ve - test.matrix @ ve))
+    print(np.sum(np.tile(ve[:, i], (a, 1)).T * ve, axis=0))
+print(np.abs(ve @ np.diag(va) - test.matrix @ ve))
 
 print("Rayleigh")
-a = 6
+a = randint(5, 10)
 test = Operator(a, random(), random())
-va, ve, di, gg, te = test.eigenalgo(10 ** -14, 10000, "Rayleigh")
+va, ve, di, gg, te = test.eigenalgo(10 ** -14, 20000, "Rayleigh")
 for i in range(a):
-    print(np.sum(np.tile(ve[i, :], (a, 1)).T * ve, axis=0))
-    print(np.abs(ve - test.matrix @ ve))
+    print(np.sum(np.tile(ve[:, i], (a, 1)).T * ve, axis=0))
+print(np.abs(ve @ np.diag(va) - test.matrix @ ve))
+
