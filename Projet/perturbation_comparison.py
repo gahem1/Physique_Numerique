9243a7c036ca998367ffe5b06f8ff341e3ca676a
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-05-06 15:22:39 -0400

    Update perturbation_comparison.py

--- Projet/perturbation_comparison.py
+++ Projet/perturbation_comparison.py
@@ -1,20 +1,48 @@
 from hamiltonian import *
 from perturbation import Perturbation
 import matplotlib.pyplot as plt
-import seaborn as
+import seaborn as sns
 
 dim = 10
-num = 200
+num = 50
+denom = 10000
 va, val = np.empty((num, num, dim)), np.empty((num, num, dim))
-
+ve, vec = np.empty((num, num, dim, dim)), np.empty((num, num, dim, dim))
 for i in range(num):
     for j in range(num):
-        test1 = Operator(dim, i / 2000, j / 2000)
-        test2 = Perturbation(dim, i / 2000, j / 2000)
-        va[i, j, :], ve, di, gg, te = test1.eigenalgo(10 ** -12, 99999, "Givens")
+        test1 = Operator(dim, i / denom, j / denom)
+        test2 = Perturbation(dim, i / denom, j / denom)
+        va[i, j, ::-1], ve[i, j, ::-1, ::-1] = (test1.eigenalgo(10 ** -10, 99999, "Givens"))[:2]
         val[i, j, :] = test2.second_order_energy() + test2.first_order_energy() + np.arange(0.5, dim + 0.5)
+        fos = test2.first_order_state()
+        vectors = np.diag(np.ones(dim)) + fos + test2.second_order_state(fos)
+        norms = np.sqrt(np.sum(vectors * vectors, axis=0))
+        for k in range(dim):
+            vectors[:, k] = vectors[:, k] / norms[k]
 
+        vec[i, j, :, :] = vectors
 
+data1 = np.sum(np.abs(va - val), axis=2) / dim
+data2 = np.sum(np.abs(ve - vec)) / dim ** 2
 
-print(val)
-print(va)
+ax = sns.heatmap(data1, cbar_kws={'label': 'Average energy difference with 2nd order perturbation theory'})
+ax.set_xlabel("beta", fontsize=18)
+ax.set_ylabel("alpha", fontsize=18)
+ax.set_xticks(np.arange(0, num, 10) + 0.5)
+ax.set_xticklabels(np.arange(0, num, 10) / denom, fontsize=18)
+ax.set_yticks(np.arange(0, num, 10) + 0.5)
+ax.set_yticklabels(np.arange(0, num, 10) / denom, fontsize=18)
+ax.invert_yaxis()
+ax.figure.axes[-1].yaxis.label.set_size(18)
+plt.show()
+
+ax = sns.heatmap(data2, cbar_kws={'label': 'Average component difference with 2nd order perturbation theory'})
+ax.set_xlabel("beta", fontsize=18)
+ax.set_ylabel("alpha", fontsize=18)
+ax.set_xticks(np.arange(0, num, 10) + 0.5)
+ax.set_xticklabels(np.arange(0, num, 10) / denom, fontsize=18)
+ax.set_yticks(np.arange(0, num, 10) + 0.5)
+ax.set_yticklabels(np.arange(0, num, 10) / denom, fontsize=18)
+ax.invert_yaxis()
+ax.figure.axes[-1].yaxis.label.set_size(18)
+plt.show()
