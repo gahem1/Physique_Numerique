dbaa4a7dd7591f3fafe2f1dfacb476ba9c76e20c
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-05-05 22:10:24 -0400

    Hu

--- Projet/perturbation.py
+++ Projet/perturbation.py
@@ -4,8 +4,9 @@ import numpy as np
 class Perturbation:
     def __init__(self, dim: int, alpha: float, beta: float):
         self.N, self.H, self.W = dim, np.diag(np.arange(0.5, dim + 0.5)), np.zeros((dim, dim))
-        self.P = np.copy(self.W)
+        self.P, self.coeff = np.copy(self.W), None
         if beta == 0 or beta == alpha ** 2:
+            self.coeff = alpha
             fa = np.arange(1, dim - 2)  # smallest factor, used to optimize matrix initialization
             self.W[3:, :-3] += np.diag(np.sqrt(fa * (fa + 1) * (fa + 2)))
 
@@ -29,6 +30,7 @@ class Perturbation:
                 self.P += np.diag(3 * (2 * fa * (fa + 1) + 1))
 
         elif alpha == 0 or alpha == beta ** 2:
+            self.coeff = beta
             fa = np.arange(1, dim - 3)
             self.W[4:, :-4] += np.diag(np.sqrt(fa * (fa + 1) * (fa + 2) * (fa + 3)))
 
@@ -52,9 +54,10 @@ class Perturbation:
                 self.P += self.P.T
 
         else:
-            coeff = beta / alpha  # Coefficient by which X^4 is multiplied
+            self.coeff = alpha
+            multiple = beta / alpha  # Coefficient by which X^4 is multiplied
             fa = np.arange(1, dim - 3)
-            self.W[4:, :-4] += np.diag(coeff * np.sqrt(fa * (fa + 1) * (fa + 2) * (fa + 3)))
+            self.W[4:, :-4] += np.diag(multiple * np.sqrt(fa * (fa + 1) * (fa + 2) * (fa + 3)))
 
             fa = np.arange(1, dim - 2)
             self.W[3:, :-3] += np.diag(np.sqrt(fa * (fa + 1) * (fa + 2)))
@@ -62,7 +65,7 @@ class Perturbation:
             fa = np.arange(dim - 2)
             com = np.sqrt(fa + 1)
             ter = fa * np.sqrt(fa + 2) + (fa + 1) * np.sqrt(fa + 2) + (fa + 2) ** (3 / 2) + (fa + 3) * np.sqrt(fa + 2)
-            self.W[2:, :-2] += np.diag(coeff * com * ter)
+            self.W[2:, :-2] += np.diag(multiple * com * ter)
 
             fa = np.arange(dim - 1)
             com = np.sqrt(fa + 1)
@@ -71,10 +74,10 @@ class Perturbation:
             self.W += self.W.T
 
             fa = np.arange(dim)
-            self.W += np.diag(3 * coeff * (2 * fa * (fa + 1) + 1))
+            self.W += np.diag(3 * multiple * (2 * fa * (fa + 1) + 1))
 
     def first_order_energy(self):
-        return np.diag(self.W)
+        return self.coeff * np.diag(self.W)
 
     def first_order_state(self):
         states = np.zeros((self.N, self.N))
@@ -82,10 +85,23 @@ class Perturbation:
             for j in range(self.N):
                 if i != j:
                     states[i, j] = self.W[i, j] / (j - i)
-        return states
+        return self.coeff * states
 
     def second_order_energy(self):
-        pass
+        energies = np.zeros(self.N)
+        for j in range(self.N):
+            for i in range(self.N):
+                if i != j:
+                    energies[j] += self.W[i, j] ** 2 / (j - i)
+        energies += np.diag(self.P)
+        return self.coeff ** 2 * energies
 
-    def second_order_state(self):
-        pass
+    def second_order_state(self, fos):
+        states = np.zeros((self.N, self.N))
+        for i in range(self.N):
+            for j in range(self.N):
+                if i != j:
+                    states[i, j] = self.P[i, j] / (j - i)
+                    states[i, j] += self.W @ fos[:, ]
+
+        return self.coeff ** 2 * states
