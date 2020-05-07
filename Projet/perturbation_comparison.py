47c284194b1bc34cb8e9f95943bb9ac4fe3c6c8c
gahem1 <59939369+gahem1@users.noreply.github.com>
2020-05-06 20:03:04 -0400

    HEY

--- Projet/perturbation_comparison.py
+++ Projet/perturbation_comparison.py
@@ -3,31 +3,36 @@ from perturbation import Perturbation
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dim = 10
+dim = 8
 num = 50
 denom = 10000
 va, val = np.empty((num, num, dim)), np.empty((num, num, dim))
-ve, vec = np.empty((num, num, dim, dim)), np.empty((num, num, dim, dim))
+ve, vec = np.empty((num, dim, dim)), np.empty((num, dim, dim))
 for i in range(num):
     for j in range(num):
         test1 = Operator(dim, i / denom, j / denom)
         test2 = Perturbation(dim, i / denom, j / denom)
-        va[i, j, ::-1], ve[i, j, ::-1, ::-1] = (test1.eigenalgo(10 ** -10, 99999, "Givens"))[:2]
-        val[i, j, :] = test2.second_order_energy() + test2.first_order_energy() + np.arange(0.5, dim + 0.5)
-        fos = test2.first_order_state()
-        vectors = np.diag(np.ones(dim)) + fos + test2.second_order_state(fos)
-        norms = np.sqrt(np.sum(vectors * vectors, axis=0))
-        for k in range(dim):
-            vectors[:, k] = vectors[:, k] / norms[k]
+        if i == 10:
+            va[i, j, :], ve[j, :, :] = (test1.eigenalgo(10 ** -12, 99999, "Rayleigh"))[:2]
+            fos = test2.first_order_state()
+            vectors = np.diag(np.ones(dim)) + fos + test2.second_order_state(fos)
+            norms = np.sqrt(np.sum(vectors * vectors, axis=0))
+            for k in range(dim):
+                vectors[:, k] = vectors[:, k] / norms[k]
+            vec[j, :, :] = vectors
+        else:
+            va[i, j, :] = (test1.eigenalgo(10 ** -10, 99999, "Rayleigh"))[0]
 
-        vec[i, j, :, :] = vectors
+        val[i, j, :] = test2.second_order_energy() + test2.first_order_energy() + np.arange(0.5, dim + 0.5)
 
 data1 = np.sum(np.abs(va - val), axis=2) / dim
-data2 = np.sum(np.abs(ve - vec)) / dim ** 2
+data2 = np.abs(va[20, :, :] - val[20, :, :])
+data3 = np.sum(np.abs(ve - vec), axis=2) / dim
 
+plt.figure()
 ax = sns.heatmap(data1, cbar_kws={'label': 'Average energy difference with 2nd order perturbation theory'})
-ax.set_xlabel("beta", fontsize=18)
-ax.set_ylabel("alpha", fontsize=18)
+ax.set_xlabel("alpha", fontsize=18)
+ax.set_ylabel("beta", fontsize=18)
 ax.set_xticks(np.arange(0, num, 10) + 0.5)
 ax.set_xticklabels(np.arange(0, num, 10) / denom, fontsize=18)
 ax.set_yticks(np.arange(0, num, 10) + 0.5)
@@ -36,13 +41,26 @@ ax.invert_yaxis()
 ax.figure.axes[-1].yaxis.label.set_size(18)
 plt.show()
 
-ax = sns.heatmap(data2, cbar_kws={'label': 'Average component difference with 2nd order perturbation theory'})
+plt.figure()
+ax = sns.heatmap(data2.T, cbar_kws={'label': 'Energy difference with 2nd order perturbation theory'})
 ax.set_xlabel("beta", fontsize=18)
-ax.set_ylabel("alpha", fontsize=18)
+ax.set_ylabel("Energy level", fontsize=18)
 ax.set_xticks(np.arange(0, num, 10) + 0.5)
 ax.set_xticklabels(np.arange(0, num, 10) / denom, fontsize=18)
-ax.set_yticks(np.arange(0, num, 10) + 0.5)
-ax.set_yticklabels(np.arange(0, num, 10) / denom, fontsize=18)
+ax.set_yticks(np.arange(dim) + 0.5)
+ax.set_yticklabels(np.arange(dim), fontsize=18)
+ax.invert_yaxis()
+ax.figure.axes[-1].yaxis.label.set_size(18)
+plt.show()
+
+plt.figure()
+ax = sns.heatmap(data3.T, cbar_kws={'label': 'Component difference with 2nd order perturbation theory'})
+ax.set_xlabel("beta", fontsize=18)
+ax.set_ylabel("Energy level", fontsize=18)
+ax.set_xticks(np.arange(0, num, 10) + 0.5)
+ax.set_xticklabels(np.arange(0, num, 10) / denom, fontsize=18)
+ax.set_yticks(np.arange(dim) + 0.5)
+ax.set_yticklabels(np.arange(dim), fontsize=18)
 ax.invert_yaxis()
 ax.figure.axes[-1].yaxis.label.set_size(18)
 plt.show()
--- Projet/spectrum_shift.py
+++ Projet/spectrum_shift.py
@@ -1,2 +1,27 @@
 from hamiltonian import *
+import matplotlib.pyplot as plt
 
+num = 3
+dim = 18
+axis = np.arange(dim)
+en = np.empty((num, num, dim))
+for i in range(num):
+    for j in range(num):
+        test = Operator(dim, (i + 1) * 0.005, (j + 1) * 0.005)
+        en[i, j, ::-1] = (test.eigenalgo(10 ** -12, 99999999, "Givens"))[0]
+
+list_of_markers = ['.', '*', '1']
+
+plt.figure()
+for i in range(num):
+    k = 0
+    for j in range(num):
+        plt.plot(axis, en[i, j, :], list_of_markers[k], markersize=8)
+        k += 1
+
+plt.xlabel("Niveau d'énergie", fontsize=18)
+plt.ylabel("Énergie", fontsize=18)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.legend(("alpha=0.005 beta=0.005", "alpha=0.005 beta=0.01", "alpha=0.005 beta=0.015", "alpha=0.01 beta=0.005", "alpha=0.01 beta=0.01", "alpha=0.01 beta=0.015", "alpha=0.015 beta=0.005", "alpha=0.015 beta=0.01", "alpha=0.015 beta=0.015"), fontsize=14)
+plt.show()
